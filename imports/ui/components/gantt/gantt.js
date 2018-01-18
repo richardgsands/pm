68fff0893f7e7460b2d1ebb56304d9f6a4ee75c3
Richard Sands <rsands@oxts.com>
2018-01-18 10:41:04 +0000

    gantt (in progress) - project added to chart

--- imports/ui/components/gantt/gantt.js
+++ imports/ui/components/gantt/gantt.js
@@ -1,27 +1,32 @@
 import './gantt.html';
+import moment from 'moment';
 
 Template.gantt.onRendered(function() {
     gantt.init("gantt_container");
+
+    let projects = [ this.data ];
+    gantt.parse({ data: _getGanttDataForProjects(projects) });
+
 });
 
 Template.gantt.helpers({
 
-    ganttData(projects) {
-        let ganttTasks = [];
 
-        projects.forEach((p) => {
-
-            ganttTasks.push({
-                id: p._id,
-                text: p.displayCodeAndName(),
-                start_date: moment(p.start_date).format("YYYY-MM-DD hh:mm:ss"),
-                duration: p.effort || 1,
-                progress: p.progress,
-                parent: null
-            })
-
-        });
-
-    }
 
 });
+
+function _getGanttDataForProjects(projects) {
+    let ganttTasks = [];
+    debugger
+    projects.forEach((p) => {
+        ganttTasks.push({
+            id: p._id,
+            text: p.displayCodeAndName(),
+            start_date: moment(p.start_date).format("DD-MM-YYYY"),
+            duration: p.effort || 1,
+            progress: p.progress,
+            parent: null
+        })
+    });
+    return ganttTasks;
+}
