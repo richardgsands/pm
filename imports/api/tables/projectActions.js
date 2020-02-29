42f272e8287bda25db5f44cded02ba1eea5beaf8
Richard Sands <rsands@oxts.com>
2020-02-29 10:13:00 +0000

    tabular owner display (project actions)

--- imports/api/tables/projectActions.js
+++ imports/api/tables/projectActions.js
@@ -1,4 +1,5 @@
 import ProjectActions from '../collections/projectActions.js';
+import ApiCommon from '../api-common';
 import Tabular from 'meteor/aldeed:tabular';
 import moment from 'moment';
 
@@ -9,9 +10,16 @@ new Tabular.Table({
   columns: [
     {data: "_order", visible: false},   // for sorting
     {data: "status", title: "Status"},
+    ApiCommon.TabularGetColumn('description', 'Action'),
     {data: "description", title: "Action"},
     {data: "effort", title: "Effort (days)"},
-    {data: "(o = this.getOwner()) ? o.displayName() : (No owner)", title: "Owner"},
+    {
+      data: "owner",
+      title: "Owner",
+      render: function (val, type, doc) {
+        return ( o = doc.getOwner() ) ? o.displayInitials() : "(No owner)";
+      }
+    },
     {
       data: "dueDate",
       title: "Due",
@@ -36,7 +44,7 @@ new Tabular.Table({
     }
   ],
   extraFields: ['projectId', 'ownerId', '_order', 'milestone', 'gateId'],  // NB: data for edit form is dependent on subscription created by table, 
-                                                                // i.e. include all fields that need to be fetched for edit form in columns or extraFields
+                                                                           // i.e. include all fields that need to be fetched for edit form in columns or extraFields
 
   // DataTables options
   paging: false,
